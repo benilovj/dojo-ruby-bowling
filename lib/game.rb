97d63eaf3073fb4ad5d13cb769ba0edd0e02c936
Jake Benilov <benilov@gmail.com>
2011-08-07 20:30:56 +0200

    handling spares

--- lib/game.rb
+++ lib/game.rb
@@ -1,53 +1,39 @@
-
-
 module Parse
-
-    def parse(score_line)
-      frames = []
-      score_line.chars.each_slice(2) {|a, b| frames << Frame.new(a, b)}
-      frames
-    end
-
+  def parse(score_line)
+    score_line.chars.each_slice(2).map {|a,b| Frame.new(a, b)}
+  end
 end
 
 class Frame
-  def initialize(roll1, roll2)
-    
+  attr_writer :next_frame
+  
+  def initialize(first_roll, second_roll)
+    @first_roll = first_roll
+    @second_roll = second_roll
+  end
+  
+  def first_roll
+    @first_roll.to_i
+  end
+  
+  def score
+    if @second_roll == "/"
+      10 + @next_frame.first_roll
+    else
+      @first_roll.to_i + @second_roll.to_i
+    end
   end
 end
 
 class Game
-  
   def score(score_line)
-    
     frames = parse(score_line)
+    frames.each_cons(2) {|current_frame, next_frame| current_frame.next_frame = next_frame}
     frames.inject(0) do |total_score, frame|
       total_score + frame.score
     end
-    
-    # score_line.chars.inject(0) do |sum, roll_char|
-    #   sum + Roll.new(roll_char).score
-    # end
   end
   
   private
   include Parse
-  
-  
-end
-
-
-
-class Roll
-  attr_reader :score
-  
-  def initialize roll_char
-    @score = score_of(roll_char)
-  end
-  
-  private
-  
-  def score_of(roll_char)
-    roll_char == "-" ? 0 : roll_char.to_i
-  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,34 +1,57 @@
 require 'game'
 
 describe Game do
-  
   before(:each) do
     @game = Game.new
   end
   
   it "should score all gutter balls as 0" do
     @game.score("--------------------").should == 0
-    
   end
   
   it "should score single rolls in each frame" do
     @game.score("9-"*10).should == 90
   end
   
-  it "should score a spare" do
-    pending
+  it "should score a spare without points in the next roll" do
     @game.score("5/" + "--"*9).should == 10 
   end
-  
-   
+
+  it "should score a spare with points in the next roll" do
+    @game.score("5/5-" + "--"*8).should == 20
+  end
 end
 
 describe Parse do
   include Parse
   
   it "should parse score line into frames" do
-    parse("--").should have(1).item
-    parse("----").should have(2).items
-    
+    parse("--").should have(1).frame
+    parse("----").should have(2).frame
   end
+end
+
+describe Frame do
+  it "should score gutter rolls" do
+    Frame.new("-", "-").score.should == 0
+  end
+
+  it "should score a frame with one gutter roll" do
+    Frame.new("5", "-").score.should == 5
+  end
+  
+  it "should score a frame with pins standing" do
+    Frame.new("1","2").score.should == 3
+  end
+  
+  it "should score a spare frame" do
+    current_frame = Frame.new("3", "/")
+    current_frame.next_frame = Frame.new("5", "-")
+    current_frame.score.should == 15
+  end
+  
+  # it "should score a strike frame" do
+  #   current_frame = Frame.new("X")
+  #   current_frame.next_frame
+  # end
 end
